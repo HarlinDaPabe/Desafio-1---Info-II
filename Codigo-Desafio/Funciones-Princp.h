60b7cb9ba7aeb52e951883f5984934c7085654ab
Harlin David <hapbe23@gmail.com>
2025-09-23 19:16:40 -0500

    Reestablecimiento Archivo

---
+++ Codigo-Desafio/Funciones-Princp.h
@@ -0,0 +1,33 @@
+#ifndef FUNCIONES_PRINCIPALES_H
+#define FUNCIONES_PRINCIPALES_H
+
+#include <iostream>
+
+using namespace std;
+
+char* decompresor_LZ78(const char* comp, int comp_len, int& output_len, int& input_used) {
+    // Inicializar variables
+    output_len = 0;
+    input_used = 0;
+
+     // Capacidad inicial para el diccionario
+    int dict_capacity = 256;
+    int dict_size = 1; // Empezamos en 1 porque la entrada 0 es vacía
+
+    // Crear diccionario
+    char** dictionary = new char*[dict_capacity];
+    int* dict_lengths = new int[dict_capacity];
+
+    // Entrada 0: cadena vacía
+    dictionary[0] = new char[1];
+    dictionary[0][0] = '\0';
+    dict_lengths[0] = 0;
+
+    // Buffer para la salida (tamaño inicial)
+    int output_capacity = comp_len * 2; // Estimación inicial
+    char* output = new char[output_capacity];
+
+    int i = 0; // Posición en la entrada comprimida
+}
+
+#endif // FUNCIONES_PRINCIPALES_H
